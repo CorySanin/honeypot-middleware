11d378a712cfafdb3056cbd5dee36d8f179bf662
Cory Sanin <corysanin@outlook.com>
2025-12-03 23:11:48 -0500

    robust xmlrpc emulation

--- honeypot-middleware.go
+++ honeypot-middleware.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	phpinfo__html        = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
+	phpinfo__html             = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 <style type="text/css">
 body {background-color: #ffffff; color: #000000;}
@@ -961,7 +961,7 @@ This program is free software; you can redistribute it and/or modify it under th
 </td></tr>
 </table><br />
 </div></body></html>`
-	wlwmanifest__xml     = `<?xml version="1.0" encoding="utf-8" ?>
+	wlwmanifest__xml          = `<?xml version="1.0" encoding="utf-8" ?>
 
 <manifest xmlns="http://schemas.microsoft.com/wlw/manifest/weblog">
 
@@ -970,7 +970,7 @@ This program is free software; you can redistribute it and/or modify it under th
 	<supportsKeywords>Yes</supportsKeywords>
 	<supportsGetTags>Yes</supportsGetTags>
   </options>
-  
+
   <weblog>
     <serviceName>WordPress</serviceName>
     <imageUrl>images/wlw/wp-icon.png</imageUrl>
@@ -978,13 +978,13 @@ This program is free software; you can redistribute it and/or modify it under th
     <homepageLinkText>View site</homepageLinkText>
     <adminLinkText>Dashboard</adminLinkText>
     <adminUrl>
-      <![CDATA[ 
-			{blog-postapi-url}/../wp-admin/ 
+      <![CDATA[
+			{blog-postapi-url}/../wp-admin/
 		]]>
     </adminUrl>
     <postEditingUrl>
-      <![CDATA[ 
-			{blog-postapi-url}/../wp-admin/post.php?action=edit&post={post-id} 
+      <![CDATA[
+			{blog-postapi-url}/../wp-admin/post.php?action=edit&post={post-id}
 		]]>
     </postEditingUrl>
   </weblog>
@@ -995,7 +995,7 @@ This program is free software; you can redistribute it and/or modify it under th
       <text>Manage Comments</text>
       <imageUrl>images/wlw/wp-comments.png</imageUrl>
       <clickUrl>
-        <![CDATA[ 
+        <![CDATA[
 				{blog-postapi-url}/../wp-admin/edit-comments.php
 			]]>
       </clickUrl>
@@ -1005,7 +1005,119 @@ This program is free software; you can redistribute it and/or modify it under th
 
 </manifest>
 `
-	dot__env             = `APP_NAME="www"
+	xmlrpc_get                = `XML-RPC server accepts POST requests only.`
+	xmlrpc_post_getUserBlogs  = `<?xml version="1.0" encoding="UTF-8"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <array>
+          <data>
+            <value>
+              <array>
+                <data>
+                  <value>
+                    <array>
+                      <data>
+                        <value>
+                          <struct>
+                            <member>
+                              <name>isAdmin</name>
+                              <value>
+                                <boolean>1</boolean>
+                              </value>
+                            </member>
+                            <member>
+                              <name>url</name>
+                              <value>
+                                <string>%REQ_BASE_URL%/</string>
+                              </value>
+                            </member>
+                            <member>
+                              <name>blogid</name>
+                              <value>
+                                <string>1</string>
+                              </value>
+                            </member>
+                            <member>
+                              <name>blogName</name>
+                              <value>
+                                <string>My Blog</string>
+                              </value>
+                            </member>
+                            <member>
+                              <name>xmlrpc</name>
+                              <value>
+                                <string>%REQ_FULL%</string>
+                              </value>
+                            </member>
+                          </struct>
+                        </value>
+                      </data>
+                    </array>
+                  </value>
+                </data>
+              </array>
+            </value>
+          </data>
+        </array>
+      </value>
+    </param>
+  </params>
+</methodResponse>`
+	xmlrpc_post_incorrect     = `<?xml version="1.0" encoding="UTF-8"?>
+<methodResponse>
+  <fault>
+    <value>
+      <struct>
+        <member>
+          <name>faultCode</name>
+          <value>
+            <int>403</int>
+          </value>
+        </member>
+        <member>
+          <name>faultString</name>
+          <value>
+            <string>Incorrect username or password.</string>
+          </value>
+        </member>
+      </struct>
+    </value>
+  </fault>
+</methodResponse>`
+	xmlrpc_post_newPost       = `<?xml version="1.0" encoding="UTF-8"?>
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <string>5419</string>
+      </value>
+    </param>
+  </params>
+</methodResponse>`
+	xmlrpc_post_notWellFormed = `<?xml version="1.0" encoding="UTF-8"?>
+<methodResponse>
+  <fault>
+    <value>
+      <struct>
+        <member>
+          <name>faultCode</name>
+          <value>
+            <int>-32700</int>
+          </value>
+        </member>
+        <member>
+          <name>faultString</name>
+          <value>
+            <string>parse error. not well formed</string>
+          </value>
+        </member>
+      </struct>
+    </value>
+  </fault>
+</methodResponse>`
+	dot__env                  = `APP_NAME="www"
 APP_ENV=production
 APP_DEBUG=false
 
@@ -1027,16 +1139,18 @@ REDIS_PORT=6379
 
 JWT_SECRET=abcdefg
 `
-	blank                = ""
-	htmlContentType      = "text/html; charset=UTF-8"
-	plaintextContentType = "text/plain"
-	xmlContentType       = "application/xml"
+	blank                     = ""
+	htmlContentType           = "text/html; charset=UTF-8"
+	plaintextContentType      = "text/plain"
+	xmlContentType            = "application/xml"
 )
 
 type Config struct {
 	Verbose             bool     `json:"verbose" yaml:"verbose" toml:"verbose"`
+	TrustProxy          bool     `json:"trustProxy" yaml:"TrustProxy" toml:"TrustProxy"`
 	PhpInfoPatterns     []string `json:"phpInfoPatterns" yaml:"phpInfoPatterns" toml:"phpInfoPatterns"`
 	ExecutionPatterns   []string `json:"executionPatterns" yaml:"executionPatterns" toml:"executionPatterns"`
+	XmlRpcPatterns      []string `json:"xmlRpcPatterns" yaml:"xmlRpcPatterns" toml:"xmlRpcPatterns"`
 	DotEnvPatterns      []string `json:"dotEnvPatterns" yaml:"dotEnvPatterns" toml:"dotEnvPatterns"`
 	WlwmanifestPatterns []string `json:"wlwmanifestPatterns" yaml:"wlwmanifestPatterns" toml:"wlwmanifestPatterns"`
 }
@@ -1044,8 +1158,10 @@ type Config struct {
 func CreateConfig() *Config {
 	return &Config{
 		Verbose:             true,
+		TrustProxy:          false,
 		PhpInfoPatterns:     []string{"/server-info\\.php$", "/(php_)?version\\.php$", "/phpinfo[0-9]?\\.php$", "/pi\\.php$", "/[^/]*\\.phpinfo$"},
-		ExecutionPatterns:   []string{"/xmlrpc\\.php$", "/function\\.php$", "/bolt\\.php$", "/env\\.php$", "/userfuns\\.php$", "/postnews\\.php$", "/pwnd\\.php$", "/init-help/init\\.php$"},
+		ExecutionPatterns:   []string{"/function\\.php$", "/bolt\\.php$", "/env\\.php$", "/userfuns\\.php$", "/postnews\\.php$", "/pwnd\\.php$", "/init-help/init\\.php$"},
+		XmlRpcPatterns:      []string{"/xmlrpc\\.php$"},
 		DotEnvPatterns:      []string{"\\.env$"},
 		WlwmanifestPatterns: []string{"/wlwmanifest\\.xml$"},
 	}
@@ -1054,43 +1170,77 @@ func CreateConfig() *Config {
 type HoneypotMiddleware struct {
 	next                http.Handler
 	Verbose             bool
+	TrustProxy          bool
 	PhpInfoPatterns     []*regexp.Regexp
 	ExecutionPatterns   []*regexp.Regexp
+	XmlRpcPatterns      []*regexp.Regexp
 	DotEnvPatterns      []*regexp.Regexp
 	WlwmanifestPatterns []*regexp.Regexp
 }
 
-func ReplaceReq(html string, path string) string {
-	return strings.ReplaceAll(html, "%REQ_PATH%", path)
+func (a *HoneypotMiddleware) getBaseURL(req *http.Request) string {
+	scheme := "http"
+	if req.TLS != nil {
+		scheme = "https"
+	}
+
+	if proto := req.Header.Get("X-Forwarded-Proto"); proto != "" && a.TrustProxy {
+		scheme = proto
+	}
+
+	return scheme + "://" + req.Host
 }
 
-func LogBody(req *http.Request) error {
-	if req.Body == nil {
-		return nil
-	}
+func (a *HoneypotMiddleware) ReplaceRuntimeVariables(html string, req *http.Request) string {
+	baseUrl := a.getBaseURL(req)
+	full := baseUrl + req.URL.Path
+	reqPath := strings.ReplaceAll(html, "%REQ_PATH%", req.URL.Path)
+	reqHost := strings.ReplaceAll(reqPath, "%REQ_HOST%", req.Host)
+	reqBaseUrl := strings.ReplaceAll(reqHost, "%REQ_BASE_URL%", baseUrl)
+	reqFull := strings.ReplaceAll(reqBaseUrl, "%REQ_FULL%", full)
+	return reqFull
+}
 
+func GetBody(req *http.Request) (string, error) {
+	if req.Body == nil {
+		return "", nil
+	}
 	bodyBytes, err := io.ReadAll(req.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	var body = string(bodyBytes)
+	return string(bodyBytes), nil
+}
+
+func LogBody(req *http.Request) (string, error) {
+	body, err := GetBody(req)
+	if err != nil {
+		return "", err
+	}
 
 	if len(body) > 0 {
 		fmt.Fprintf(os.Stdout, "[honeypot🟢] Body received: %s\n", body)
 	}
 
-	return nil
+	return body, nil
+}
+
+func (a *HoneypotMiddleware) GetRemoteAddr(req *http.Request) string {
+	if ip := req.Header.Get("CF-Connecting-IP"); ip != "" && a.TrustProxy {
+		return ip
+	}
+	return req.RemoteAddr
 }
 
 func (a *HoneypotMiddleware) SendResponse(rw http.ResponseWriter, req *http.Request, str string, contentType string) {
 	rw.Header().Set("Content-type", contentType)
 	rw.Header().Set("X-Robots-Tag", "noindex")
 	rw.WriteHeader(200)
-	rw.Write([]byte(ReplaceReq(str, req.URL.Path)))
+	rw.Write([]byte(a.ReplaceRuntimeVariables(str, req)))
 	if a.Verbose {
-		fmt.Fprintf(os.Stdout, "[honeypot🟢] serving %s:'%s%s' to %s UA: '%s'\n", req.Method, req.Host, req.RequestURI, req.RemoteAddr, req.UserAgent())
-		var err = LogBody(req)
+		fmt.Fprintf(os.Stdout, "[honeypot🟢] serving %s:'%s%s' to %s UA: '%s'\n", req.Method, req.Host, req.RequestURI, a.GetRemoteAddr(req), req.UserAgent())
+		_, err := LogBody(req)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "[honeypot🔴] Failed to read request body: %s\n", err)
 		}
@@ -1112,6 +1262,30 @@ func (a *HoneypotMiddleware) ServeHTTP(rw http.ResponseWriter, req *http.Request
 		rw.Header().Set("X-Powered-By", "PHP/5.3.29")
 		rw.Header().Set("Server", "Apache/2.4.10 (Debian)")
 		a.SendResponse(rw, req, phpinfo__html, htmlContentType)
+		return
+	}
+	if IsMatch(req, a.XmlRpcPatterns) {
+		if req.Method == "GET" {
+			a.SendResponse(rw, req, xmlrpc_get, xmlContentType)
+			return
+		}
+		body, err := LogBody(req)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[honeypot🔴] Failed to read request body: %s\n", err)
+		}
+		if strings.Count(body, "admin") >= 2 {
+			a.SendResponse(rw, req, xmlrpc_post_incorrect, xmlContentType)
+			return
+		}
+		if strings.Contains(body, "wp.getUsersBlogs") {
+			a.SendResponse(rw, req, xmlrpc_post_getUserBlogs, xmlContentType)
+			return
+		}
+		if strings.Contains(body, "metaWeblog.newPost") {
+			a.SendResponse(rw, req, xmlrpc_post_newPost, xmlContentType)
+			return
+		}
+		a.SendResponse(rw, req, xmlrpc_post_notWellFormed, xmlContentType)
 		return
 	}
 	if IsMatch(req, a.ExecutionPatterns) {
@@ -1159,11 +1333,17 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	if err != nil {
 		return nil, err
 	}
+	XmlRpcPatterns, err := MakeRegexSlice(config.XmlRpcPatterns)
+	if err != nil {
+		return nil, err
+	}
 	return &HoneypotMiddleware{
 		next:                next,
 		Verbose:             config.Verbose,
+		TrustProxy:          config.TrustProxy,
 		PhpInfoPatterns:     phpInfoPatterns,
 		ExecutionPatterns:   executionPatterns,
+		XmlRpcPatterns:      XmlRpcPatterns,
 		DotEnvPatterns:      dotEnvPatterns,
 		WlwmanifestPatterns: wlwmanifestPatterns,
 	}, nil
--- preprocessor/plugin-template.go
+++ preprocessor/plugin-template.go
@@ -11,19 +11,26 @@ import (
 )
 
 const (
-	phpinfo__html        = `<%- files['phpinfo.html'] %>`
-	wlwmanifest__xml     = `<%- files['wlwmanifest.xml'] %>`
-	dot__env             = `<%- files['dot.env'] %>`
-	blank                = ""
-	htmlContentType      = "text/html; charset=UTF-8"
-	plaintextContentType = "text/plain"
-	xmlContentType       = "application/xml"
+	phpinfo__html             = `<%- files['phpinfo.html'] %>`
+	wlwmanifest__xml          = `<%- files['wlwmanifest.xml'] %>`
+	xmlrpc_get                = `<%- files['xmlrpc.get.txt'] %>`
+	xmlrpc_post_getUserBlogs  = `<%- files['xmlrpc.post-get-user-blogs.xml'] %>`
+	xmlrpc_post_incorrect     = `<%- files['xmlrpc.post-incorrect.xml'] %>`
+	xmlrpc_post_newPost       = `<%- files['xmlrpc.post.new-post.xml'] %>`
+	xmlrpc_post_notWellFormed = `<%- files['xmlrpc.post.not-well-formed.xml'] %>`
+	dot__env                  = `<%- files['dot.env'] %>`
+	blank                     = ""
+	htmlContentType           = "text/html; charset=UTF-8"
+	plaintextContentType      = "text/plain"
+	xmlContentType            = "application/xml"
 )
 
 type Config struct {
 	Verbose             bool     `json:"verbose" yaml:"verbose" toml:"verbose"`
+	TrustProxy          bool     `json:"trustProxy" yaml:"TrustProxy" toml:"TrustProxy"`
 	PhpInfoPatterns     []string `json:"phpInfoPatterns" yaml:"phpInfoPatterns" toml:"phpInfoPatterns"`
 	ExecutionPatterns   []string `json:"executionPatterns" yaml:"executionPatterns" toml:"executionPatterns"`
+	XmlRpcPatterns      []string `json:"xmlRpcPatterns" yaml:"xmlRpcPatterns" toml:"xmlRpcPatterns"`
 	DotEnvPatterns      []string `json:"dotEnvPatterns" yaml:"dotEnvPatterns" toml:"dotEnvPatterns"`
 	WlwmanifestPatterns []string `json:"wlwmanifestPatterns" yaml:"wlwmanifestPatterns" toml:"wlwmanifestPatterns"`
 }
@@ -31,8 +38,10 @@ type Config struct {
 func CreateConfig() *Config {
 	return &Config{
 		Verbose:             true,
+		TrustProxy:          false,
 		PhpInfoPatterns:     []string{"/server-info\\.php$", "/(php_)?version\\.php$", "/phpinfo[0-9]?\\.php$", "/pi\\.php$", "/[^/]*\\.phpinfo$"},
-		ExecutionPatterns:   []string{"/xmlrpc\\.php$", "/function\\.php$", "/bolt\\.php$", "/env\\.php$", "/userfuns\\.php$", "/postnews\\.php$", "/pwnd\\.php$", "/init-help/init\\.php$"},
+		ExecutionPatterns:   []string{"/function\\.php$", "/bolt\\.php$", "/env\\.php$", "/userfuns\\.php$", "/postnews\\.php$", "/pwnd\\.php$", "/init-help/init\\.php$"},
+		XmlRpcPatterns:      []string{"/xmlrpc\\.php$"},
 		DotEnvPatterns:      []string{"\\.env$"},
 		WlwmanifestPatterns: []string{"/wlwmanifest\\.xml$"},
 	}
@@ -41,43 +50,77 @@ func CreateConfig() *Config {
 type HoneypotMiddleware struct {
 	next                http.Handler
 	Verbose             bool
+	TrustProxy          bool
 	PhpInfoPatterns     []*regexp.Regexp
 	ExecutionPatterns   []*regexp.Regexp
+	XmlRpcPatterns      []*regexp.Regexp
 	DotEnvPatterns      []*regexp.Regexp
 	WlwmanifestPatterns []*regexp.Regexp
 }
 
-func ReplaceReq(html string, path string) string {
-	return strings.ReplaceAll(html, "%REQ_PATH%", path)
+func (a *HoneypotMiddleware) getBaseURL(req *http.Request) string {
+	scheme := "http"
+	if req.TLS != nil {
+		scheme = "https"
+	}
+
+	if proto := req.Header.Get("X-Forwarded-Proto"); proto != "" && a.TrustProxy {
+		scheme = proto
+	}
+
+	return scheme + "://" + req.Host
 }
 
-func LogBody(req *http.Request) error {
-	if req.Body == nil {
-		return nil
-	}
+func (a *HoneypotMiddleware) ReplaceRuntimeVariables(html string, req *http.Request) string {
+	baseUrl := a.getBaseURL(req)
+	full := baseUrl + req.URL.Path
+	reqPath := strings.ReplaceAll(html, "%REQ_PATH%", req.URL.Path)
+	reqHost := strings.ReplaceAll(reqPath, "%REQ_HOST%", req.Host)
+	reqBaseUrl := strings.ReplaceAll(reqHost, "%REQ_BASE_URL%", baseUrl)
+	reqFull := strings.ReplaceAll(reqBaseUrl, "%REQ_FULL%", full)
+	return reqFull
+}
 
+func GetBody(req *http.Request) (string, error) {
+	if req.Body == nil {
+		return "", nil
+	}
 	bodyBytes, err := io.ReadAll(req.Body)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	var body = string(bodyBytes)
+	return string(bodyBytes), nil
+}
+
+func LogBody(req *http.Request) (string, error) {
+	body, err := GetBody(req)
+	if err != nil {
+		return "", err
+	}
 
 	if len(body) > 0 {
 		fmt.Fprintf(os.Stdout, "[honeypot🟢] Body received: %s\n", body)
 	}
 
-	return nil
+	return body, nil
+}
+
+func (a *HoneypotMiddleware) GetRemoteAddr(req *http.Request) string {
+	if ip := req.Header.Get("CF-Connecting-IP"); ip != "" && a.TrustProxy {
+		return ip
+	}
+	return req.RemoteAddr
 }
 
 func (a *HoneypotMiddleware) SendResponse(rw http.ResponseWriter, req *http.Request, str string, contentType string) {
 	rw.Header().Set("Content-type", contentType)
 	rw.Header().Set("X-Robots-Tag", "noindex")
 	rw.WriteHeader(200)
-	rw.Write([]byte(ReplaceReq(str, req.URL.Path)))
+	rw.Write([]byte(a.ReplaceRuntimeVariables(str, req)))
 	if a.Verbose {
-		fmt.Fprintf(os.Stdout, "[honeypot🟢] serving %s:'%s%s' to %s UA: '%s'\n", req.Method, req.Host, req.RequestURI, req.RemoteAddr, req.UserAgent())
-		var err = LogBody(req)
+		fmt.Fprintf(os.Stdout, "[honeypot🟢] serving %s:'%s%s' to %s UA: '%s'\n", req.Method, req.Host, req.RequestURI, a.GetRemoteAddr(req), req.UserAgent())
+		_, err := LogBody(req)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "[honeypot🔴] Failed to read request body: %s\n", err)
 		}
@@ -99,6 +142,30 @@ func (a *HoneypotMiddleware) ServeHTTP(rw http.ResponseWriter, req *http.Request
 		rw.Header().Set("X-Powered-By", "PHP/5.3.29")
 		rw.Header().Set("Server", "Apache/2.4.10 (Debian)")
 		a.SendResponse(rw, req, phpinfo__html, htmlContentType)
+		return
+	}
+	if IsMatch(req, a.XmlRpcPatterns) {
+		if req.Method == "GET" {
+			a.SendResponse(rw, req, xmlrpc_get, xmlContentType)
+			return
+		}
+		body, err := LogBody(req)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[honeypot🔴] Failed to read request body: %s\n", err)
+		}
+		if strings.Count(body, "admin") >= 2 {
+			a.SendResponse(rw, req, xmlrpc_post_incorrect, xmlContentType)
+			return
+		}
+		if strings.Contains(body, "wp.getUsersBlogs") {
+			a.SendResponse(rw, req, xmlrpc_post_getUserBlogs, xmlContentType)
+			return
+		}
+		if strings.Contains(body, "metaWeblog.newPost") {
+			a.SendResponse(rw, req, xmlrpc_post_newPost, xmlContentType)
+			return
+		}
+		a.SendResponse(rw, req, xmlrpc_post_notWellFormed, xmlContentType)
 		return
 	}
 	if IsMatch(req, a.ExecutionPatterns) {
@@ -146,11 +213,17 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	if err != nil {
 		return nil, err
 	}
+	XmlRpcPatterns, err := MakeRegexSlice(config.XmlRpcPatterns)
+	if err != nil {
+		return nil, err
+	}
 	return &HoneypotMiddleware{
 		next:                next,
 		Verbose:             config.Verbose,
+		TrustProxy:          config.TrustProxy,
 		PhpInfoPatterns:     phpInfoPatterns,
 		ExecutionPatterns:   executionPatterns,
+		XmlRpcPatterns:      XmlRpcPatterns,
 		DotEnvPatterns:      dotEnvPatterns,
 		WlwmanifestPatterns: wlwmanifestPatterns,
 	}, nil
